771743a5b06c445988af093e75ec8822625defe7
Jindrich K. Smitka <jsmitka@smita.info>
2018-05-14 12:53:55 +0200

    Added base documentation

--- lib/image_plug_parser.ex
+++ lib/image_plug_parser.ex
@@ -1,6 +1,57 @@
 defmodule ImagePlugParser do
   @moduledoc """
-  Parses image in request body.
+  Parses image in PUT request body.
+
+  ## Usage
+
+  Image parser must be used only in controller
+
+      defmodule Server.Web.FilesController do
+        use Server.Web, :controller
+
+        plug Plug.Parsers,
+          parsers: [ImagePlugParser],
+          subtypes: ["jpeg", "png"]
+
+        def upload(conn, %{"file" => %{filename: filename, path: path, content_type: mimetype}}) do
+          path
+          |> FilesManager.upload(filename, mimetype)
+          |> response(conn)
+        end
+      end
+
+  and router must set wildcard path with name `filename`
+
+      defmodule Server.Web.Router do
+        use Server.Web, :router
+
+        pipeline :api do
+          plug :accepts, ["json"]
+        end
+
+        scope "/api", Server.Web do
+          pipe_through :api
+
+          put "/files/:page_id/*filename", FilesController, :upload
+        end
+      end
+
+  ## Options
+
+    * `:subtypes` - list of supported image content types, eg. jpeg or png
+
+  All options supported by `Plug.Conn.read_body/2` are also supported here.
+  They are repeated here for convenience:
+
+    * `:length` - sets the maximum number of bytes to read from the request,
+      defaults to 8_000_000 bytes
+    * `:read_length` - sets the amount of bytes to read at one time from the
+      underlying socket to fill the chunk, defaults to 1_000_000 bytes
+    * `:read_timeout` - sets the timeout for each socket read, defaults to
+      15_000ms
+
+  So by default, `Plug.Parsers` will read 1_000_000 bytes at a time from the
+  socket with an overall limit of 8_000_000 bytes.
   """
 
   alias Plug.{Conn, Upload}
